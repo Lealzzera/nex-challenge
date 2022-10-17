75106d5b23a380ef3d2d08756c4ac5e2044a6ddd
Lealzzera <matheus.mleall@gmail.com>
2022-10-16 21:19:36 -0300

    creating and configuring the screen funcionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import HomeView from './views/HomeView/HomeView';
 import LoginView from './views/LoginView/LoginView';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<LoginView />} />
-      <Route path='/home' element={<HomeView />} />
     </Routes>
   )
 }
